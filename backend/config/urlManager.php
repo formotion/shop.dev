decae792eeaa7687958d3d772e10da041a47fbdf
formotion <angeluse@mail.ru>
2017-11-02 22:09:49 +0700

    Create cross-cutting links to send messages and obtain the absolute address.

---
+++ backend/config/urlManager.php
@@ -0,0 +1,17 @@
+<?php
+
+return[
+    'class' => 'yii\web\UrlManager',
+    
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
+        '<_a:loin:logout>' => 'site/<_a>',
+
+    '<_c:[\w\-]+>' => '<_c>/index',
+    '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+    '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+    '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+        
+];    
