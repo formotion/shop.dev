efe7fdc7622c7bc68dbd942523545ab53fe8e94c
formotion <angeluse@mail.ru>
2017-11-12 18:36:12 +0700

    Added social login controller

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'cokieDomain'=> 'example.ru',
-    
+    'user.rememberMeDuration' => 3600 * 24 * 30,
+    'cookieDomain' => 'example.com',
+
 ];
