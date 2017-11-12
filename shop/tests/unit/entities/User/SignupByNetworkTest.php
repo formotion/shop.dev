085b82e8f68d47f13d9871b48f03f537adb07b26
formotion <angeluse@mail.ru>
2017-11-12 17:43:12 +0700

    Added user networks model

--- shop/tests/unit/entities/User/SignupByNetworkTest.php
+++ shop/tests/unit/entities/User/SignupByNetworkTest.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace shop\tests\unit\entities\User;
-
 use Codeception\Test\Unit;
 use shop\entities\User\User;
-
 class SignupByNetworkTest extends Unit
 {
     public function testSuccess()
@@ -13,9 +10,7 @@ class SignupByNetworkTest extends Unit
             $network = 'vk',
             $identity = '123456'
         );
-
         $this->assertCount(1, $networks = $user->networks);
-
         $this->assertEquals($identity, $networks[0]->identity);
         $this->assertEquals($network, $networks[0]->network);
         $this->assertNotEmpty($user->created_at);
