c45c9cd4138be57b5c3f981ab4ae36d258c57247
formotion <angeluse@mail.ru>
2017-11-18 15:11:02 +0700

    Changed columns list

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 /* @var $this yii\web\View */
-/* @var $model shop\entities\User\User */
+/* @var $model shop\entities\User */
 $this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
@@ -22,20 +22,19 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
-            'email:email',
-            'email_confirm_token:email',
-            'status',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
-
+    <div class="box">
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'id',
+                    'username',
+                    'email:email',
+                    'status',
+                    'created_at:datetime',
+                    'updated_at:datetime',
+                ],
+            ]) ?>
+        </div>
+    </div>
 </div>
