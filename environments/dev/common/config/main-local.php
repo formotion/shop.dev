d57f740851726fbcf2d863518f12a10b72c49120
formotion <angeluse@mail.ru>
2017-11-19 12:22:28 +0700

    Added brands management

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=shop',
+            'dsn' => 'mysql:host=localhost;dbname=Shop',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
