7f52ab7b508ef1abd1016c43ac8c6e3822bb94a4
formotion <angeluse@mail.ru>
2017-11-19 17:01:54 +0700

    Installed image upload behavior

--- shop/entities/Shop/Product/Photo.php
+++ shop/entities/Shop/Product/Photo.php
@@ -1,11 +1,17 @@
 <?php
+
 namespace shop\entities\Shop\Product;
+
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
+use yiidreamteam\upload\ImageUploadBehavior;
+
 /**
  * @property integer $id
  * @property string $file
  * @property integer $sort
+ *
+ * @mixin ImageUploadBehavior
  */
 class Photo extends ActiveRecord
 {
@@ -15,16 +21,38 @@ class Photo extends ActiveRecord
         $photo->file = $file;
         return $photo;
     }
+
     public function setSort($sort): void
     {
         $this->sort = $sort;
     }
+
     public function isIdEqualTo($id): bool
     {
         return $this->id == $id;
     }
+
     public static function tableName(): string
     {
         return '{{%shop_photos}}';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            [
+                'class' => ImageUploadBehavior::className(),
+                'attribute' => 'file',
+                'createThumbsOnRequest' => true,
+                'filePath' => '@staticRoot/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'fileUrl' => '@static/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'thumbPath' => '@staticRoot/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbUrl' => '@static/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbs' => [
+                    'admin' => ['width' => 100, 'height' => 70],
+                    'thumb' => ['width' => 640, 'height' => 480],
+                ],
+            ],
+        ];
     }
 }
